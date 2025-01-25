45b793b6f8b8dcb4b3b47b43ce2bbd2591ca5adf
Maritza Diaz <maritzadiaz77@gmail.com>
2025-01-24 18:14:31 -0800

    added essential files and modifed app,main,Header, and footer files

---
+++ components/App.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Nav from "./components/NavTabs";
+
+import "./App.css";
+
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,1 +1,18 @@
+// import linkedinlogo from '../assets/linkedinlogo.png';
+// import githublogo from '../assets/githublogo.png';
+import "../App.css";
 
+export default function Footer() {
+  return (
+    <>
+      <h3>My Links</h3>
+      <a href="https://www.linkedin.com/in/maritza-diaz-65379b15a/">
+        {/* <img className="icon" title="Linked-In" src={linkedinlogo}/> */}
+      </a>
+
+      <a href="https://github.com/maritzadiaz77">
+        {/* <img  className="icon" title="GitHub" src={githublogo}/> */}
+      </a>
+    </>
+  );
+}
