80cbf45d9dc9bf44c795c0da4b8651b3dbba18f8
Maritza Diaz <maritzadiaz77@gmail.com>
2025-01-24 18:35:43 -0800

    worked on pages and jsx files

---
+++ pages/Portfolio.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+export default function Portfolio() {
+  return (
+    <>
+      <h2>This is my Portfolio!</h2>
+      <div>
+        {/* <a href="renderlinkhere/">
+          <img className="portpicture" src={mmprologo} />
+        </a> */}
+        {/* <a href="githublinkhere">
+          <br></br>
+          <button className="ghbutton">MM Github Repostitory</button>
+        </a> */}
+      </div>
+      <div>
+        {/* <a href="renderlinkhereee">
+          <img className="portpicture" src={lalogo} />
+        </a>
+        <a href="githublinkhereee">
+          <br></br>
+          <button className="ghbutton">A Github Repostitory!!</button>
+        </a> */}
+      </div>
+    </>
+  );
+}
