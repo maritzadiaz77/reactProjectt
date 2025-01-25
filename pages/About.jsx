b2b69df56dc6d899c4688f1bb241b90ab3b5df36
Maritza Diaz <maritzadiaz77@gmail.com>
2025-01-24 18:21:38 -0800

    Added an about section page

---
+++ pages/About.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <>
+      <h2>About Me:</h2>
+      <h4>
+        Dear Visitor, All of this is random text All of this is random text All
+        of this is random text All of this is random text All of this is random
+        textAll of this is random text All of this is random text All of this is
+        random text All of this is random text All of this is random textAll of
+        this is random text All of this is random text All of this is random
+        text All of this is random text All of this is random textAll of this is
+        random text All of this is random text All of this is random text All of
+        this is random text Warm regards
+      </h4>
+    </>
+  );
+}
